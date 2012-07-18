48573a80d84a70e52806eaad93dd4c19379dbb14
Yannick Morin <ymorin007@gmail.com>
2012-07-18 19:17:58 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mersea::Application.config.secret_token = '36723eb5c9349d2e26f6cbde821c0d88257633e7acc44ba5f656e2ee09cc841294b86604577abc30a7c1ff7a2d5bd7c6da474ba4a542e0de0d3478f1fb7a85db'
