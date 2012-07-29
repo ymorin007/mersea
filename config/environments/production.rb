4cd6ddf98af0f1cc36c3a68913c229b9d56e8b48
Yannick Morin <ymorin007@gmail.com>
2012-07-29 14:40:25 -0400

    m

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,11 +11,9 @@ Mersea::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
-
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # Heroku requires thats
+  # http://vincentopensourcetaiwan.blogspot.com/2012/07/heroku-actionviewtemplateerror.html
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
