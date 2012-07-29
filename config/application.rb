4bc5592127484b73f4ec6729c03624ad3201aab7
Yannick Morin <ymorin007@gmail.com>
2012-07-29 13:18:32 -0400

    1

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,11 @@ module Mersea
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.precompile += %w( main.css  )
+    
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile=false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
